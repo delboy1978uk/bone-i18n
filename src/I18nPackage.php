3a3f857f1218389990d0994715f6a259595e429a
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-02-26 16:34:58 +0100

    add translator to package\

--- src/I18nPackage.php
+++ src/I18nPackage.php
@@ -6,6 +6,11 @@ namespace Bone\I18n;
 
 use Barnacle\Container;
 use Barnacle\RegistrationInterface;
+use Bone\Mvc\View\Extension\Plates\LocaleLink;
+use Bone\Mvc\View\Extension\Plates\Translate;
+use Bone\Mvc\View\PlatesEngine;
+use Bone\Service\TranslatorFactory;
+use Laminas\I18n\Translator\Translator;
 
 class I18nPackage implements RegistrationInterface
 {
@@ -14,6 +19,20 @@ class I18nPackage implements RegistrationInterface
      */
     public function addToContainer(Container $c)
     {
+        $config = $c->has('i18n') ? $c->get('i18n') : [
+
+        ];
+        $engine = $c->get(PlatesEngine::class);
+
+        if (is_array($config)) {
+            $factory = new TranslatorFactory();
+            $translator = $factory->createTranslator($config);
+            $engine->loadExtension(new Translate($translator));
+            $engine->loadExtension(new LocaleLink());
+            $defaultLocale = $config['default_locale'] ?: 'en_GB';
+            $translator->setLocale($defaultLocale);
+            $c[Translator::class] = $translator;
+        }
     }
 
     /**
